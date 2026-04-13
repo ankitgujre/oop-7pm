3f37c2732b8dc02bd519ef1de8e385f42c646703
Ankit Gujre <ankitgujre374@gmail.com>
2026-04-13 20:15:01 +0530

    problems question

---
+++ problemQuestions2.py
@@ -0,0 +1,93 @@
+
+# print even 1 to 20 and odd 1 to 20
+
+# for i in range(1, 21):
+#     if i % 2 == 0:
+#         print(i, "is even")
+#     else:
+#         print(i, "is odd")
+
+# print table
+
+# reverse number
+
+
+
+
+# for i in range(10, 0, -1):
+#     print(i)
+
+# n = int(input("Enter a number: "))
+# factorial = 1
+# for i in range(1, n + 1):
+#     factorial *= i
+# print("The factorial of", n, "is:", factorial)
+
+'''
+print each character of a string in a new line
+
+count vowels in a string
+
+
+
+'''
+
+# st = input("Enter a string: ")
+# vowels = "aeiouAEIOU"
+# count = 0
+# for char in st:
+#     print(char)
+#     if char in vowels:
+#         count += 1
+# print("The number of vowels in the string is:", count)
+
+'''
+find the largest and smallest number in a list
+'''
+# numbers = [5, 2, 8, 1, 9, 3]
+# largest = numbers[0]
+
+# for n in numbers:
+#     if n > largest:
+#         largest = n
+# print("The largest number is:", largest)
+
+'''
+find the smallest number in a list
+
+print square of numbers from 1 to 10
+
+print cube of numbers from 1 to 10
+
+skip even numbers from 1 to 20
+'''
+# for i in range(1, 21):
+
+
+    # if i % 2 == 0:
+    #     continue
+    # print(i)
+
+
+# reverse a string
+
+# st = input("Enter a string: ")
+# reverse = ""
+# for char in st:
+#     reverse = char + reverse
+# print("The reverse of the string is:", reverse)
+
+''' count digits in a number '''
+
+# num = int(input("Enter a number: "))
+# count = 0
+# while num > 0:
+#     num //= 10
+#     count += 1
+# print("The number of digits in the number is:", count)
+
+''' find the sum of the digits of a number '''
+
+n = 5
+for i in range(n):
+    print("*" * n)
